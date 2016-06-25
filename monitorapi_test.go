8d4e335645ca468e5d178a5650e29d744bd7e5b9
Simon Tucker <brotherlogic@gmail.com>
2016-06-25 14:55:29 -0700

    Added endpoint to get all heartbeats. This closes #13.

--- monitorapi_test.go
+++ monitorapi_test.go
@@ -3,9 +3,29 @@ package main
 import (
 	"golang.org/x/net/context"
 	"testing"
+	"time"
 
 	pbr "github.com/brotherlogic/discovery/proto"
+	pb "github.com/brotherlogic/monitor/proto"
 )
+
+func TestHeatBeatTime(t *testing.T) {
+	s := InitServer()
+	r, _ := s.ReceiveHeartbeat(context.Background(), &pbr.RegistryEntry{Identifier: "Blah", Name: "Test"})
+
+	firstTime := r.BeatTime
+	time.Sleep(1 * time.Second)
+	s.ReceiveHeartbeat(context.Background(), &pbr.RegistryEntry{Identifier: "Blah", Name: "Test"})
+
+	r3, _ := s.GetHeartbeats(context.Background(), &pb.Empty{})
+	if len(r3.Beats) != 1 {
+		t.Errorf("Too many heartbeats")
+	}
+
+	if r3.GetBeats()[0].BeatTime == firstTime {
+		t.Errorf("Time has not updated")
+	}
+}
 
 func TestHeartBeat(t *testing.T) {
 	s := InitServer()
@@ -16,5 +36,14 @@ func TestHeartBeat(t *testing.T) {
 
 	if r.BeatTime <= 0 {
 		t.Errorf("Failed to get the right beat time: %v", r.BeatTime)
+	}
+
+	r2, err := s.GetHeartbeats(context.Background(), &pb.Empty{})
+	if err != nil {
+		t.Errorf("Unable to get list of heartbeats: %v", err)
+	}
+
+	if len(r2.Beats) == 0 {
+		t.Errorf("No heartbeats received")
 	}
 }
