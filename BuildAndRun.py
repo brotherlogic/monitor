9d599ce405a8e0676cbcf3bf383838d30cae154b
Simon Tucker <brotherlogic@gmail.com>
2016-06-30 21:20:07 -0700

    Working on the build script

--- BuildAndRun.py
+++ BuildAndRun.py
@@ -1,12 +1,17 @@
 import os
 import subprocess
 
+name = "monitor"
+
+current_hash = os.popen('git rev-parse HEAD').readlines()[0]
 # Update to the latest version
-for line in os.popen('go get -u github.com/brotherlogic/monitor').readlines():
+for line in os.popen('go get -u github.com/brotherlogic/' + name).readlines():
     print line.strip()
+new_hash = os.popen('git rev-parse HEAD').readlines()[0]
+
     
 # Move the old version over
-for line in os.popen('cp monitor oldmonitor').readlines():
+for line in os.popen('cp ' + name + ' old' + name).readlines():
     print line.strip()
 
 # Rebuild
@@ -17,11 +22,13 @@ for line in os.popen('go build ./...').readlines():
 for line in os.popen('go build').readlines():
     print line.strip()
 
+size_1 = os.path.getsize('./old' + name)
+size_2 = os.path.getsize('./' + name)
 
-size_1 = os.path.getsize('./oldmonitor')
-size_2 = os.path.getsize('./monitor')
+running = len(os.popen('ps -ef | grep ' + name).readlines()) > 2
 
-if size_1 != size_2:
-    for line in os.popen('killall monitor').readlines():
+              
+if size_1 != size_2 or new_hash != current_hash or not running:
+    for line in os.popen('killall ' + name).readlines():
         pass
-    subprocess.Popen('./monitor')
+    subprocess.Popen(['./' + name, '--sync=false'])
