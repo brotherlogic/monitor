156589719fd1665cb4db0773a036aa4ff783345b
Simon Tucker <brotherlogic@gmail.com>
2018-02-23 18:17:46 -0800

    Rips heartbeats out of monitor. This closes #367.

--- monitorapi.go
+++ monitorapi.go
@@ -22,7 +22,6 @@ type Issuer interface {
 // Server the main server type
 type Server struct {
 	*goserver.GoServer
-	heartbeats    []*pb.Heartbeat
 	logDirectory  string
 	stats         []*pb.Stats
 	logs          []*pb.MessageLog
@@ -50,17 +49,10 @@ func (s *Server) emailSlowFunction() {
 
 // InitServer creates a monitoring server
 func InitServer() *Server {
-	s := &Server{&goserver.GoServer{}, make([]*pb.Heartbeat, 0), "logs", make([]*pb.Stats, 0), make([]*pb.MessageLog, 0), ProdIssuer{}, time.Now(), &sync.Mutex{}}
+	s := &Server{&goserver.GoServer{}, "logs", make([]*pb.Stats, 0), make([]*pb.MessageLog, 0), ProdIssuer{}, time.Now(), &sync.Mutex{}}
 	s.issuer = ProdIssuer{Resolver: s.GetIP}
 	s.Register = s
 	return s
-}
-
-// ReceiveHeartbeat receives a heartbeat from another server
-func (s *Server) ReceiveHeartbeat(ctx context.Context, in *pbr.RegistryEntry) (*pb.Heartbeat, error) {
-	heartbeat := &pb.Heartbeat{Entry: in, BeatTime: time.Now().Unix()}
-	s.heartbeats = append(s.heartbeats, heartbeat)
-	return heartbeat, nil
 }
 
 // WriteMessageLog Writes out a message log
@@ -143,29 +135,4 @@ func (s *Server) WriteFunctionCall(ctx context.Context, in *pb.FunctionCall) (*p
 	}
 
 	return &pb.Empty{}, nil
-}
-
-// GetHeartbeats gets the list of per job heartbeats
-func (s *Server) GetHeartbeats(ctx context.Context, in *pb.Empty) (*pb.HeartbeatList, error) {
-	var mapper map[string]*pb.Heartbeat
-	mapper = make(map[string]*pb.Heartbeat)
-
-	for _, heartbeat := range s.heartbeats {
-		name := heartbeat.Entry.Identifier + "/" + heartbeat.Entry.Name
-		if _, ok := mapper[name]; ok {
-			if heartbeat.BeatTime > mapper[name].BeatTime {
-				mapper[name] = heartbeat
-			}
-		} else {
-			mapper[name] = heartbeat
-
-		}
-	}
-
-	result := &pb.HeartbeatList{}
-	for _, beat := range mapper {
-		result.Beats = append(result.Beats, beat)
-	}
-
-	return result, nil
 }
